e9a54e102ffb3bfa0cedfd93717dea5d0f0a6966
David jimmy junior <davidjimmyjunior@gmail.com>
2024-02-26 08:18:33 +0100

    week 3 assignment

--- week 2 assignment.py
+++ week 2 assignment.py
@@ -14,3 +14,42 @@ my_list.sort()
 print(my_list)
 index_30 = my_list.index(30)
 print(index_30)
+
+
+def outer_fun(a, b):
+    def inner_fun(c, d):
+        return c + d
+    return inner_fun(a, b)
+
+res = outer_fun(5, 10)
+print(res)
+
+def display(**kwargs):
+    for i in kwargs:
+        print(i)
+
+display(emp="Kelly", salary=9000)
+
+def add(a, b):
+    return a+5, b+5
+
+result = add(3, 2)
+print(result)
+
+for num in range(1, 5):
+    print(num)
+"""
+    def some_thing(number1, number2):
+    first_value = number1 + 8
+    second_value = number2 - 5
+    return first_value
+
+print(some_thing(13, 10))
+"""
+x = 0
+while x < 10:
+    x = x + 1
+
+ x = 0
+for i in range(3):
+    x = x + i   
---
+++ week 3 assignment.py
@@ -0,0 +1,26 @@
+"""
+Create a function named calculate_discount(price, discount_percent) that calculates the final price
+after applying a discount. The function should take the original price (price) and the discount percentage (discount_percent) as 
+parameters. If the discount is 20% or higher, apply the discount; otherwise, return the original 
+price.
+Using the calculate_discount function, prompt the user to enter the original price of an item and 
+the discount percentage. Print the final price after applying the discount, or if no discount was 
+applied, print the original price.
+"""
+
+
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        final_price = price - (price * discount_percent / 100)
+        return final_price
+    else:
+        return price
+
+original_price = float(input("Enter the original price of the item: "))
+discount = float(input("Enter the discount percentage: "))
+
+final_price = calculate_discount(original_price, discount)
+if final_price == original_price:
+    print("No discount applied. Original price: $", original_price)
+else:
+    print("Final price after discount: $", final_price)
